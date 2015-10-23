0d49bd3c5704e89f4e17bcab4a35903394d1f9c0
mlabouardy <mohamed.labouardy@gmail.com>
2015-10-23 18:55:35 +0200

    init database with roles

--- scrum-tool/src/main/java/com/labouardy/service/InitService.java
+++ scrum-tool/src/main/java/com/labouardy/service/InitService.java
@@ -6,6 +6,8 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.labouardy.entity.Role;
+
 @Service
 @Transactional
 public class InitService {
@@ -15,6 +17,17 @@ public class InitService {
 	
 	@PostConstruct
 	public void init(){
+		Role product_owner=new Role();
+		product_owner.setName("Product Owner");
 		
+		Role scrum_master=new Role();
+		scrum_master.setName("Scrum Master");
+		
+		Role scrum_team=new Role();
+		scrum_team.setName("Scrum Team");
+		
+		roleService.save(product_owner);
+		roleService.save(scrum_master);
+		roleService.save(scrum_team);
 	}
 }
