9b968339f4f03a1f4996a68626332c952d408b7b
N'zi Etranse Landry-Desire <landry.desire.nzi@live.fr>
2015-11-21 13:11:59 +0100

    Assignation Task Scrum Team

--- scrum-tool/src/main/java/com/bordeaux/entity/user/ScrumTeam.java
+++ scrum-tool/src/main/java/com/bordeaux/entity/user/ScrumTeam.java
@@ -1,8 +1,14 @@
 package com.bordeaux.entity.user;
 
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
 
 import com.bordeaux.entity.Role.RoleType;
+import com.bordeaux.entity.Task;
 
 @Entity
 public class ScrumTeam extends User{
@@ -11,4 +17,18 @@ public class ScrumTeam extends User{
 		this.setRole(RoleType.TEAM.getRole());
 	}
 	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Collection<Task> tasks;
+	
+	public ScrumTeam(int id){
+		super(id);
+	}
+	
+	public Collection<Task> getTasks() {
+		return tasks;
+	}
+	
+	public void setTasks(Collection<Task> tasks) {
+	this.tasks = tasks;
+	}
 }
